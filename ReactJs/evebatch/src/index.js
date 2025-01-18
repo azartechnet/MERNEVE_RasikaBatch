ac68a7eb9e06baacfea64f8c03d5bc00b64d5bbd
azartechnet <azartechnet@gmail.com>
2025-01-18 17:49:54 +0530

    updated

--- ReactJs/evebatch/src/index.js
+++ ReactJs/evebatch/src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//Basic Rendering
+/*const App=()=>{
+  return(
+    <div>
+      <h1>React App</h1>
+      <p>This is a paragraph</p>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<App/>)*/
+//Function Rendering
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*function Sample()
+{
+  return(
+    <div>
+      <h1>Sample App</h1>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample/>)*/
+
+//Dynamic Rendering
+
+const App=()=>{
+  const name="Rahul"
+  return(
+    <div>
+      <h1>Dynamic App</h1>
+      <p>My name is {name}</p>
+      <p>Today date:{new Date().toDateString()}</p>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<App/>)
