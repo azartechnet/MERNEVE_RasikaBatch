d2c1b2535655f6cb57484f95a71ae7d5abf98986
azartechnet <azartechnet@gmail.com>
2025-02-26 18:58:16 +0530

    updated

---
+++ ReactJs/navigateprj/src/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Home()
+{
+    const navigate = useNavigate();
+    const goToAbout=()=>{
+        navigate("/about");
+    }
+    const goToContact=()=>{
+        navigate("/contact");
+        }
+        return (
+            <div>
+                <h1>Home Page</h1>
+                <button onClick={goToAbout}>About</button>
+                <button onClick={goToContact}>Contact</button>
+            </div>
+            );
+}
+export default Home;
